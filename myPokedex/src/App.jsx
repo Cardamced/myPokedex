8271cb42296edbdfa598378651c3e96cbe0c8f7d
Cardamced <120644423+Cardamced@users.noreply.github.com>
2023-09-21 00:43:56 +0200

    added more pokemons

--- myPokedex/src/App.jsx
+++ myPokedex/src/App.jsx
@@ -7,14 +7,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   const pokemonList = [
     {
-      name: "Bulbizarre",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "charmander",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      },
+      {
+        name: "squirtle",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      },
+      {
+        name: "pikachu",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      },
+      {
+        name: "mew",
+      },
+    ];
 
   console.log(pokemonList[1].imgSrc)
   const pokemonToDisplay = pokemonList[0];
