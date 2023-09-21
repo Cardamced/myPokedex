e021b8dadc8067959e2442e0086a1cc2a33faa7c
Cardamced <120644423+Cardamced@users.noreply.github.com>
2023-09-21 10:38:55 +0200

    Add previous next and State

--- myPokedex/src/App.jsx
+++ myPokedex/src/App.jsx
@@ -2,43 +2,69 @@ import './App.css';
 import MyTitle from './Components/MyTitle';
 import PokemonCard from "./Components/PokemonCard"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 
+const pokemonList = [
+  {
+    name: "Bulbizarre",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "Charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "Squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "Pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "Mew",
+  },
+];
 
 function App() {
-  const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-      },
-      {
-        name: "charmander",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-      },
-      {
-        name: "squirtle",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-      },
-      {
-        name: "pikachu",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-      },
-      {
-        name: "mew",
-      },
-    ];
+  /* `const [pokemonIndex, setPokemonIndex] = useState(0);` utilise le hook `useState` de React pour
+  créer une variable d'état appelée `pokemonIndex` et une fonction de définition correspondante
+  appelée `setPokemonIndex`. La valeur initiale de `pokemonIndex` est définie sur 0. */
+  const [pokemonIndex, setPokemonIndex] = useState(0);
 
-  console.log(pokemonList[1].imgSrc)
-  const pokemonToDisplay = pokemonList[0];
+  const nextPokemon = () => {
+    if (pokemonIndex < pokemonList.length -1) {
+      setPokemonIndex(pokemonIndex + 1);
+    }
+  }
+
+  const previousPokemon = () => {
+    if (pokemonIndex > 0) {
+      setPokemonIndex(pokemonIndex - 1);
+    }
+  }
+
+  /* La ligne `const pokemonToDisplay = pokemonList[pokemonIndex];` attribue la valeur de
+  `pokemonList[pokemonIndex]` à la variable `pokemonToDisplay`. */
+  const pokemonToDisplay = pokemonList[pokemonIndex];
+
   return (
-    <div>
-      <MyTitle/>
-      <PokemonCard pokemon={pokemonToDisplay} />
-    </div>
-  )
+    <>
+      <div>
+        <MyTitle />
+        </* `PokemonCard pokemon={pokemonToDisplay}` transmet la valeur de `pokemonToDisplay` en tant
+        qu'accessoire appelé `pokemon` au composant `PokemonCard`. Cela permet au composant
+        `PokemonCard` d'accéder et d'utiliser les données du Pokémon actuellement affiché. */
+        PokemonCard pokemon={pokemonToDisplay} />
+        <button onClick={previousPokemon}>Précédent</button>
+        <button onClick={nextPokemon}>Suivant</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- myPokedex/src/Components/PokemonCard.jsx
+++ myPokedex/src/Components/PokemonCard.jsx
@@ -1,13 +1,10 @@
 import PropTypes from "prop-types";
 
-
-function PokemonCard(props) {
-    console.log(props); // Affiche les propriétés reçues dans la console
-    const { pokemon } = props; // déstructure la prop pokemon
+function PokemonCard({pokemon}) {
+    console.log(pokemon); // Affiche les propriétés reçues dans la console
 
     return (
         <figure>
-            {props.title}
             {/* Affiche l'image si elle est disponible (et pas undefined ?) */}
             {pokemon.imgSrc === "" || pokemon.imgSrc === undefined ? <p>???</p> : <img src={pokemon.imgSrc} />}
             {/* Affiche le nom du Pokémon via la prop */}
@@ -17,8 +14,16 @@ function PokemonCard(props) {
 
 }
 PokemonCard.propTypes = {
-    name: PropTypes.string.isRequired,
-    imgSrc: PropTypes.string.isRequired,
+    /* `pokemon: PropTypes.shape({` définit le prop `pokemon` comme un objet avec une forme ou une
+    structure spécifique. Il spécifie les propriétés attendues et leurs types pour le prop
+    `pokemon`. Dans ce cas, il attend le ` pokemon` doit avoir les propriétés `name`, `imgSrc` et
+    `initialValue`, qui doivent toutes être de type chaîne et sont obligatoires (comme indiqué par
+    `isRequired`). */
+    pokemon: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        imgSrc: PropTypes.string.isRequired,
+        initialValue: PropTypes.string.isRequired,
+    }).isRequired,
 }
 
 export default PokemonCard;
